0d3c08c52887ca865e3a662ad5c8cff83842ef4b
arttminie <parktaehyung49@gmail.com>
2022-01-12 19:55:40 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const pessoa = "Thiago"
+ 
+let number = 1957
+
+let anoAtual = 2022 - number
+console.log(anoAtual)
+
+console.log(number + anoAtual)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
